011500f283c049c36042660bced2d0e285708824
Ravi Kishan <ravikishanaka@gmail.com>
2018-08-04 15:54:25 +0530

    Added ViewModel class

---
+++ app/src/main/java/com/example/ravi/employee/ViewModel.java
@@ -0,0 +1,27 @@
+package com.example.ravi.employee;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+/**
+ * Created by Ravi on 04-08-2018.
+ */
+
+public class ViewModel extends AndroidViewModel{
+
+    private LiveData<List<Employee>> employees;
+
+    public ViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<List<Employee>> getEmployees(){
+        return employees;
+    }
+
+
+}
